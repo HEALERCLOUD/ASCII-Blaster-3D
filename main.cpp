294c27709200bd639cce15a6d8d574c212f198f2
luka akhaladze <healerluka@gmail.com>
2024-01-25 18:51:28 +0400

    added needed include files fix Cmake propertys

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-int nWindowSize = 120;
-int mWindowSize = 240;
+#include "iostream"
+#include "vector"
+#include <ncurses.h>
+#include "utility"
+#include "algorithm"
+#include "chrono"
+
+////////////////////////////////////////////////////////////
+///Global Variables
+////////////////////////////////////////////////////////////
+int n_screen_width = 140;
+int n_screen_height = 40;
 
 int main() {
 
+    //Create Screen Buffer
+    std::vector<wchar_t> screen(n_screen_height * n_screen_width);
+    initscr();
 }
