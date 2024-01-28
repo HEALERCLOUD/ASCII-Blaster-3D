38e2d1057164d21588a5548f5d0c6f6523e813cf
luka akhaladze <healerluka@gmail.com>
2024-01-28 20:50:54 +0400

    added buffer,map

--- main.cpp
+++ main.cpp
@@ -6,15 +6,46 @@
 #include "algorithm"
 #include "chrono"
 
+using  namespace std;
+
+#include <stdio.h>
+#include <Windows.h>
+
+
 ////////////////////////////////////////////////////////////
 ///Global Variables
 ////////////////////////////////////////////////////////////
 int n_screen_width = 140;
 int n_screen_height = 40;
 
+
 int main() {
 
     //Create Screen Buffer
-    std::vector<wchar_t> screen(n_screen_height * n_screen_width);
-    initscr();
+    // Create Screen Buffer
+    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    //initing map
+    wstring map;
+    map += L"#########.......";
+    map += L"#...............";
+    map += L"#.......########";
+    map += L"#..............#";
+    map += L"#......##......#";
+    map += L"#......##......#";
+    map += L"#..............#";
+    map += L"###............#";
+    map += L"##.............#";
+    map += L"#......####..###";
+    map += L"#......#.......#";
+    map += L"#......#.......#";
+    map += L"#..............#";
+    map += L"#......#########";
+    map += L"#..............#";
+    map += L"################";
+
+
 }
